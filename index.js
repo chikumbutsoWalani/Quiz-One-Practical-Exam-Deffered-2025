497d39b8bb35a9cb7fb4b5b3eab7e1de08d9e246
Chikumbutso Walani <walanchiku@gmail.com>
2025-08-04 13:08:49 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// TODO: Task 1 - Add JavaScript to toggle navigation links when hamburger menu is clicked
+
+// TODO: Task 3 - Add JavaScript to toggle light/dark theme with animation when button is clicked
+
+// TODO: Task 4 - Use jQuery to:
+// - Highlight clicked list item
+// - Clear highlights when "Clear" button is clicked
+// - Add new list items from form input without reloading the page
